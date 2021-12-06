4b0bbeb95b261af70d83363b9e14018d665c54d4
Hutu Tudor <hutu.tudor@yahoo.com>
2021-12-06 20:16:15 +0200

    feat(ast): add more simplify functions

--- src/ast/simplify.cpp
+++ src/ast/simplify.cpp
@@ -1,5 +1,6 @@
 #include "simplify.h"
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -107,6 +108,191 @@ bool rule_mul_1_lhs(node_t*& ast) {
   return true;
 }
 
+bool rule_div_1(node_t*& ast) {
+  if (!(ast->token.type == TOKEN_DIV && ast->right &&
+        ast->right->token.type == TOKEN_NUMBER && ast->right->token.val == 1)) {
+    return false;
+  }
+
+  node_t* left = clone_ast(ast->left);
+
+  destroy_ast(ast);
+
+  ast = left;
+
+  return true;
+}
+
+bool rule_add_0_rhs(node_t*& ast) {
+  if (!(ast->token.type == TOKEN_PLUS && ast->right &&
+        ast->right->token.type == TOKEN_NUMBER && ast->right->token.val == 0)) {
+    return false;
+  }
+
+  node_t* left = clone_ast(ast->left);
+
+  destroy_ast(ast);
+
+  ast = left;
+
+  return true;
+}
+
+bool rule_add_0_lhs(node_t*& ast) {
+  if (!(ast->token.type == TOKEN_PLUS && ast->left &&
+        ast->left->token.type == TOKEN_NUMBER && ast->left->token.val == 0)) {
+    return false;
+  }
+
+  node_t* right = clone_ast(ast->right);
+
+  destroy_ast(ast);
+
+  ast = right;
+
+  return true;
+}
+
+bool rule_sub_0_rhs(node_t*& ast) {
+  if (!(ast->token.type == TOKEN_MINUS && ast->right &&
+        ast->right->token.type == TOKEN_NUMBER && ast->right->token.val == 0)) {
+    return false;
+  }
+
+  node_t* left = clone_ast(ast->left);
+
+  destroy_ast(ast);
+
+  ast = left;
+
+  return true;
+}
+
+bool rule_num_add(node_t*& ast) {
+  if (!(ast->token.type == TOKEN_PLUS && ast->left &&
+        ast->left->token.type == TOKEN_NUMBER && ast->right &&
+        ast->right->token.type == TOKEN_NUMBER)) {
+    return false;
+  }
+
+  double val = ast->left->token.val + ast->right->token.val;
+
+  destroy_ast(ast);
+
+  ast = (node_t*)malloc(sizeof(node_t));
+  ast->token.type = TOKEN_NUMBER;
+  ast->token.val = val;
+
+  ast->left = nullptr;
+  ast->right = nullptr;
+
+  return true;
+}
+
+bool rule_num_sub(node_t*& ast) {
+  if (!(ast->token.type == TOKEN_MINUS && ast->left &&
+        ast->left->token.type == TOKEN_NUMBER && ast->right &&
+        ast->right->token.type == TOKEN_NUMBER)) {
+    return false;
+  }
+
+  double val = ast->left->token.val - ast->right->token.val;
+
+  destroy_ast(ast);
+
+  ast = (node_t*)malloc(sizeof(node_t));
+  ast->token.type = TOKEN_NUMBER;
+  ast->token.val = val;
+
+  ast->left = nullptr;
+  ast->right = nullptr;
+
+  return true;
+}
+
+bool rule_num_mul(node_t*& ast) {
+  if (!(ast->token.type == TOKEN_MUL && ast->left &&
+        ast->left->token.type == TOKEN_NUMBER && ast->right &&
+        ast->right->token.type == TOKEN_NUMBER)) {
+    return false;
+  }
+
+  double val = ast->left->token.val * ast->right->token.val;
+
+  destroy_ast(ast);
+
+  ast = (node_t*)malloc(sizeof(node_t));
+  ast->token.type = TOKEN_NUMBER;
+  ast->token.val = val;
+
+  ast->left = nullptr;
+  ast->right = nullptr;
+
+  return true;
+}
+
+bool rule_num_div(node_t*& ast) {
+  if (!(ast->token.type == TOKEN_DIV && ast->left &&
+        ast->left->token.type == TOKEN_NUMBER && ast->right &&
+        ast->right->token.type == TOKEN_NUMBER)) {
+    return false;
+  }
+
+  double val = ast->left->token.val / ast->right->token.val;
+
+  destroy_ast(ast);
+
+  ast = (node_t*)malloc(sizeof(node_t));
+  ast->token.type = TOKEN_NUMBER;
+  ast->token.val = val;
+
+  ast->left = nullptr;
+  ast->right = nullptr;
+
+  return true;
+}
+
+bool rule_num_pow(node_t*& ast) {
+  if (!(ast->token.type == TOKEN_POW && ast->left &&
+        ast->left->token.type == TOKEN_NUMBER && ast->right &&
+        ast->right->token.type == TOKEN_NUMBER)) {
+    return false;
+  }
+
+  double val = pow(ast->left->token.val, ast->right->token.val);
+
+  destroy_ast(ast);
+
+  ast = (node_t*)malloc(sizeof(node_t));
+  ast->token.type = TOKEN_NUMBER;
+  ast->token.val = val;
+
+  ast->left = nullptr;
+  ast->right = nullptr;
+
+  return true;
+}
+
+bool rule_num_sqrt(node_t*& ast) {
+  if (!(ast->token.type == TOKEN_SQRT && ast->left &&
+        ast->left->token.type == TOKEN_NUMBER)) {
+    return false;
+  }
+
+  double val = sqrt(ast->left->token.val);
+
+  destroy_ast(ast);
+
+  ast = (node_t*)malloc(sizeof(node_t));
+  ast->token.type = TOKEN_NUMBER;
+  ast->token.val = val;
+
+  ast->left = nullptr;
+  ast->right = nullptr;
+
+  return true;
+}
+
 void simplify_ast(node_t*& ast) {
   if (!ast) return;
 
@@ -119,4 +305,14 @@ void simplify_ast(node_t*& ast) {
   APPLY_RULE(rule_mul_0_lhs);
   APPLY_RULE(rule_mul_1_rhs);
   APPLY_RULE(rule_mul_1_lhs);
+  APPLY_RULE(rule_div_1);
+  APPLY_RULE(rule_add_0_rhs);
+  APPLY_RULE(rule_add_0_lhs);
+  APPLY_RULE(rule_sub_0_rhs);
+  APPLY_RULE(rule_num_add);
+  APPLY_RULE(rule_num_sub);
+  APPLY_RULE(rule_num_mul);
+  APPLY_RULE(rule_num_div);
+  APPLY_RULE(rule_num_pow);
+  APPLY_RULE(rule_num_sqrt);
 }
--- test/suites/simplify_ast.h
+++ test/suites/simplify_ast.h
@@ -32,11 +32,11 @@ return func_test_simplify("x^1", "x");
 TEST_END
 
 TEST_BEGIN(should_simplify_pow_1_complex)
-return func_test_simplify("(2+3)^1-7", "2 + 3 - 7");
+return func_test_simplify("(2+3)^1-7", "-2");
 TEST_END
 
 TEST_BEGIN(should_simplify_pow_1_complex_deep)
-return func_test_simplify("(2+3)^1^1-7", "2 + 3 - 7");
+return func_test_simplify("(2+3)^1^1-7", "-2");
 TEST_END
 
 TEST_BEGIN(should_simplify_pow_0_simple)
@@ -44,11 +44,11 @@ return func_test_simplify("x^0", "1");
 TEST_END
 
 TEST_BEGIN(should_simplify_pow_0_complex)
-return func_test_simplify("(2+3)^0+7", "1 + 7");
+return func_test_simplify("(2+3)^0+7", "8");
 TEST_END
 
 TEST_BEGIN(should_simplify_pow_0_complex_deep)
-return func_test_simplify("(2+3)^x^0+7", "2 + 3 + 7");
+return func_test_simplify("(2+3)^x^0+7", "12");
 TEST_END
 
 TEST_BEGIN(should_simplify_mul_0_rhs)
@@ -60,11 +60,51 @@ return func_test_simplify("0 * (2 + 5)", "0");
 TEST_END
 
 TEST_BEGIN(should_simplify_mul_1_rhs)
-return func_test_simplify("(2 + 5) * 1", "2 + 5");
+return func_test_simplify("(2 + 5) * 1", "7");
 TEST_END
 
 TEST_BEGIN(should_simplify_mul_1_lhs)
-return func_test_simplify("1 * (2 + 5)", "2 + 5");
+return func_test_simplify("1 * (2 + 5)", "7");
+TEST_END
+
+TEST_BEGIN(should_simplify_div_1)
+return func_test_simplify("(x + 3) / 1", "x + 3");
+TEST_END
+
+TEST_BEGIN(should_simplify_add_0_rhs)
+return func_test_simplify("x^5 + 0", "x ^ 5");
+TEST_END
+
+TEST_BEGIN(should_simplify_add_0_lhs)
+return func_test_simplify("0 + x^5", "x ^ 5");
+TEST_END
+
+TEST_BEGIN(should_simplify_sub_0_rhs)
+return func_test_simplify("x^5 - 0", "x ^ 5");
+TEST_END
+
+TEST_BEGIN(should_simplify_num_add)
+return func_test_simplify("4 + 5.2", "9.2");
+TEST_END
+
+TEST_BEGIN(should_simplify_num_sub)
+return func_test_simplify("4 - 5.2", "-1.2");
+TEST_END
+
+TEST_BEGIN(should_simplify_num_mul)
+return func_test_simplify("2 * 2", "4");
+TEST_END
+
+TEST_BEGIN(should_simplify_num_div)
+return func_test_simplify("8 / 4", "2");
+TEST_END
+
+TEST_BEGIN(should_simplify_num_pow)
+return func_test_simplify("2^3", "8");
+TEST_END
+
+TEST_BEGIN(should_simplify_num_sqrt)
+return func_test_simplify("sqrt9", "3");
 TEST_END
 
 SUITE_BEGIN(simplify_ast)
@@ -78,4 +118,14 @@ SUITE_ADD(should_simplify_mul_0_rhs)
 SUITE_ADD(should_simplify_mul_0_lhs)
 SUITE_ADD(should_simplify_mul_1_rhs)
 SUITE_ADD(should_simplify_mul_1_lhs)
+SUITE_ADD(should_simplify_div_1)
+SUITE_ADD(should_simplify_add_0_rhs)
+SUITE_ADD(should_simplify_add_0_lhs)
+SUITE_ADD(should_simplify_sub_0_rhs)
+SUITE_ADD(should_simplify_num_add)
+SUITE_ADD(should_simplify_num_sub)
+SUITE_ADD(should_simplify_num_mul)
+SUITE_ADD(should_simplify_num_div)
+SUITE_ADD(should_simplify_num_pow)
+SUITE_ADD(should_simplify_num_sqrt)
 SUITE_END
