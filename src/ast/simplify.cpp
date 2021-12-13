15e280f262b25f1e94d61c3bc8d831d25faaf315
Roman Cristian <romancristianroman@yahoo.com>
2021-12-13 18:01:01 +0200

    [feat]: adding tests for every derivation rule

--- src/ast/simplify.cpp
+++ src/ast/simplify.cpp
@@ -293,6 +293,180 @@ bool rule_num_sqrt(node_t*& ast) {
   return true;
 }
 
+bool rule_ln_e(node_t*& ast) {
+  if (!(ast->token.type == TOKEN_LN && ast->left &&
+        ast->left->token.type == TOKEN_E)) {
+    return false;
+  }
+
+  destroy_ast(ast);
+
+  ast = (node_t*)malloc(sizeof(node_t));
+  ast->token.type = TOKEN_NUMBER;
+  ast->token.val = 1;
+
+  ast->left = nullptr;
+  ast->right = nullptr;
+
+  return true;
+}
+
+bool rule_lg_10(node_t*& ast) {
+  if (!(ast->token.type == TOKEN_LG && ast->left &&
+        ast->left->token.type == TOKEN_NUMBER && ast->left->token.val == 10)) {
+    return false;
+  }
+
+  destroy_ast(ast);
+
+  ast = (node_t*)malloc(sizeof(node_t));
+  ast->token.type = TOKEN_NUMBER;
+  ast->token.val = 1;
+
+  ast->left = nullptr;
+  ast->right = nullptr;
+
+  return true;
+}
+
+bool rule_x_pow_nr_MUL_x_pow_nr(node_t*& ast) {
+  if (!(ast->token.type == TOKEN_MUL && ast->left && ast->right &&
+        ast->left->token.type == TOKEN_POW &&
+        ast->right->token.type == TOKEN_POW && ast->left->left &&
+        ast->left->right && ast->right->left && ast->right->right &&
+        ast->left->left->token.type == TOKEN_X &&
+        ast->left->right->token.type == TOKEN_NUMBER &&
+        ast->right->left->token.type == TOKEN_X &&
+        ast->right->right->token.type == TOKEN_NUMBER)) {
+    return false;
+  }
+
+  double pow_1 = ast->left->right->token.val;
+  double pow_2 = ast->right->right->token.val;
+
+  destroy_ast(ast);
+
+  ast = (node_t*)malloc(sizeof(node_t));
+  ast->token.type = TOKEN_POW;
+
+  ast->left = new node_t();
+  ast->left->token.type = TOKEN_X;
+
+  ast->right = new node_t();
+  ast->right->token.type = TOKEN_NUMBER;
+  ast->right->token.val = pow_1 + pow_2;
+  return true;
+}
+
+bool rule_x_pow_nr_DIV_x_pow_nr(node_t*& ast) {
+  if (!(ast->token.type == TOKEN_DIV && ast->left && ast->right &&
+        ast->left->token.type == TOKEN_POW &&
+        ast->right->token.type == TOKEN_POW && ast->left->left &&
+        ast->left->right && ast->right->left && ast->right->right &&
+        ast->left->left->token.type == TOKEN_X &&
+        ast->left->right->token.type == TOKEN_NUMBER &&
+        ast->right->left->token.type == TOKEN_X &&
+        ast->right->right->token.type == TOKEN_NUMBER)) {
+    return false;
+  }
+
+  double pow_1 = ast->left->right->token.val;
+  double pow_2 = ast->right->right->token.val;
+
+  destroy_ast(ast);
+
+  ast = (node_t*)malloc(sizeof(node_t));
+  ast->token.type = TOKEN_POW;
+
+  ast->left = new node_t();
+  ast->left->token.type = TOKEN_X;
+
+  ast->right = new node_t();
+  ast->right->token.type = TOKEN_NUMBER;
+  ast->right->token.val = pow_1 - pow_2;
+  return true;
+}
+
+bool rule_nr_pow_x_DIV_nr_pow_x(node_t*& ast) {
+  if (!(ast->token.type == TOKEN_DIV && ast->left && ast->right &&
+        ast->left->token.type == TOKEN_POW &&
+        ast->right->token.type == TOKEN_POW && ast->left->left &&
+        ast->left->right && ast->right->left && ast->right->right &&
+        ast->left->left->token.type == TOKEN_NUMBER &&
+        ast->left->right->token.type == TOKEN_X &&
+        ast->right->left->token.type == TOKEN_NUMBER &&
+        ast->right->right->token.type == TOKEN_X)) {
+    return false;
+  }
+
+  double base_1 = ast->left->left->token.val;
+  double base_2 = ast->right->left->token.val;
+
+  destroy_ast(ast);
+
+  ast = (node_t*)malloc(sizeof(node_t));
+  ast->token.type = TOKEN_POW;
+
+  ast->left = new node_t();
+  ast->left->token.type = TOKEN_NUMBER;
+  ast->left->token.val = base_1 / base_2;
+
+  ast->right = new node_t();
+  ast->right->token.type = TOKEN_X;
+  return true;
+}
+
+bool rule_nr_pow_x_MUL_nr_pow_x(node_t*& ast) {
+  if (!(ast->token.type == TOKEN_MUL && ast->left && ast->right &&
+        ast->left->token.type == TOKEN_POW &&
+        ast->right->token.type == TOKEN_POW && ast->left->left &&
+        ast->left->right && ast->right->left && ast->right->right &&
+        ast->left->left->token.type == TOKEN_NUMBER &&
+        ast->left->right->token.type == TOKEN_X &&
+        ast->right->left->token.type == TOKEN_NUMBER &&
+        ast->right->right->token.type == TOKEN_X)) {
+    return false;
+  }
+
+  double base_1 = ast->left->left->token.val;
+  double base_2 = ast->right->left->token.val;
+
+  destroy_ast(ast);
+
+  ast = (node_t*)malloc(sizeof(node_t));
+  ast->token.type = TOKEN_POW;
+
+  ast->left = new node_t();
+  ast->left->token.type = TOKEN_NUMBER;
+  ast->left->token.val = base_1 * base_2;
+
+  ast->right = new node_t();
+  ast->right->token.type = TOKEN_X;
+
+  return true;
+}
+
+bool rule_x_add_x(node_t*& ast) {
+  if (!(ast->token.type == TOKEN_PLUS && ast->left && ast->right &&
+        ast->left->token.type == TOKEN_X &&
+        ast->right->token.type == TOKEN_X)) {
+    return false;
+  }
+
+  destroy_ast(ast);
+  ast = (node_t*)malloc(sizeof(node_t));
+  ast->token.type = TOKEN_MUL;
+
+  ast->left = new node_t();
+  ast->left->token.type = TOKEN_NUMBER;
+  ast->left->token.val = 2;
+
+  ast->right = new node_t();
+  ast->right->token.type = TOKEN_X;
+
+  return true;
+}
+
 void simplify_ast(node_t*& ast) {
   if (!ast) return;
 
@@ -315,4 +489,11 @@ void simplify_ast(node_t*& ast) {
   APPLY_RULE(rule_num_div);
   APPLY_RULE(rule_num_pow);
   APPLY_RULE(rule_num_sqrt);
+  APPLY_RULE(rule_ln_e);
+  APPLY_RULE(rule_lg_10);
+  APPLY_RULE(rule_x_pow_nr_MUL_x_pow_nr);
+  APPLY_RULE(rule_x_pow_nr_DIV_x_pow_nr);
+  APPLY_RULE(rule_nr_pow_x_MUL_nr_pow_x);
+  APPLY_RULE(rule_nr_pow_x_DIV_nr_pow_x);
+  APPLY_RULE(rule_x_add_x);
 }
--- test/suites/deriv_ast.h
+++ test/suites/deriv_ast.h
@@ -23,12 +23,50 @@ bool func_test_deriv(const char* data, const char* expected) {
   return true;
 }
 
-TEST_BEGIN(should_derivate_add)
-return func_test_deriv("x-1", "1");
+TEST_BEGIN(should_derivate_x)
+return func_test_deriv("x", "1");
 TEST_END
 
-TEST_BEGIN(should_derivate_pow)
+TEST_BEGIN(should_derivate_add)
+return func_test_deriv("x+1", "1");
+TEST_END
+
+TEST_BEGIN(should_derivate_diff)
+return func_test_deriv("(x^2)-(x^3)", "2 * x ^ 1 * 1 - 3 * x ^ 2 * 1");
+TEST_END
+
+TEST_BEGIN(should_derivate_div)
+return func_test_deriv("1/x", "1 * x ^ -1");
+TEST_END
+
+TEST_BEGIN(should_derivate_mul)
+return func_test_deriv("x*x", "1 * x + x * 1");
+TEST_END
+
+TEST_BEGIN(should_derivate_pow_x_nr)
 return func_test_deriv("x^4", "4 * x ^ 3 * 1");
+TEST_END
+
+TEST_BEGIN(should_derivate_pow_nr_x)
+return func_test_deriv("4^x", "4 ^ x * ln4");
+TEST_END
+
+TEST_BEGIN(should_derivate_pow_expresion_expresion)
+return func_test_deriv("(x^2)^(3^x)",
+                       "x ^ 2 ^ 3 ^ x * (2 * x ^ 1 * 1 * 3 ^ x / x ^ 2 + 3 ^ x "
+                       "* ln3 * ln(x ^ 2))");
+TEST_END
+
+TEST_BEGIN(should_derivate_lg)
+return func_test_deriv("lgx", "1 / x * ln10");
+TEST_END
+
+TEST_BEGIN(should_derivate_ln)
+return func_test_deriv("lnx", "x ^ -1");
+TEST_END
+
+TEST_BEGIN(should_derivate_sin)
+return func_test_deriv("sin x", "cosx * 1");
 TEST_END
 
 TEST_BEGIN(should_derivate_cos)
@@ -51,12 +89,41 @@ TEST_BEGIN(should_derivate_arcsin)
 return func_test_deriv("arcsin x", "1 / sqrt(1 - x ^ 2) * 1");
 TEST_END
 
+TEST_BEGIN(should_derivate_arccos)
+return func_test_deriv("arccos x", "-1 / sqrt(1 - x ^ 2) * 1");
+TEST_END
+
+TEST_BEGIN(should_derivate_arccotan)
+return func_test_deriv("arccotan x", "-1 / (1 + x ^ 2) * 1");
+TEST_END
+
+TEST_BEGIN(should_derivate_arctan)
+return func_test_deriv("arctan x", "1 / (1 + x ^ 2) * 1");
+TEST_END
+
+TEST_BEGIN(should_derivate_error_ln_e)
+return func_test_deriv("ln e + ln x", "x ^ -1");
+TEST_END
+
 SUITE_BEGIN(deriv_ast)
+SUITE_ADD(should_derivate_x)
 SUITE_ADD(should_derivate_add)
-SUITE_ADD(should_derivate_pow)
+SUITE_ADD(should_derivate_diff)
+SUITE_ADD(should_derivate_div)
+SUITE_ADD(should_derivate_mul)
+SUITE_ADD(should_derivate_lg)
+SUITE_ADD(should_derivate_ln)
+SUITE_ADD(should_derivate_pow_x_nr)
+SUITE_ADD(should_derivate_pow_nr_x)
+SUITE_ADD(should_derivate_pow_expresion_expresion)
+SUITE_ADD(should_derivate_sin)
 SUITE_ADD(should_derivate_cos)
 SUITE_ADD(should_derivate_tan)
 SUITE_ADD(should_derivate_cotan)
 SUITE_ADD(should_derivate_sqrt)
 SUITE_ADD(should_derivate_arcsin)
+SUITE_ADD(should_derivate_arccos)
+SUITE_ADD(should_derivate_arctan)
+SUITE_ADD(should_derivate_arccotan)
+SUITE_ADD(should_derivate_error_ln_e)
 SUITE_END
