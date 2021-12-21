3a60cb5210d270c05c9b911970e0a8107244ee53
Hutu Tudor <hutu.tudor@yahoo.com>
2021-12-21 21:11:56 +0200

    feat(ast): fix paranthesis for the same operator precedence

--- src/ast/ast.cpp
+++ src/ast/ast.cpp
@@ -488,7 +488,10 @@ void dfs_ast_to_token_array(node_t* ast, token_array_t* token_array) {
 
     if (isOperator(ast->token) && ast->right && isOperator(ast->right->token) &&
         (getOperatorPrecedence(ast->token) >
-         getOperatorPrecedence(ast->right->token))) {
+             getOperatorPrecedence(ast->right->token) ||
+         (getOperatorPrecedence(ast->token) ==
+              getOperatorPrecedence(ast->right->token) &&
+          !isRightAssociative(ast->token)))) {
       push_token_array(token_array, {.type = TOKEN_L_PAREN});
       dfs_ast_to_token_array(ast->right, token_array);
       push_token_array(token_array, {.type = TOKEN_R_PAREN});
